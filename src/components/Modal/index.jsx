9d56d5eaae1b0503e2fb512b61d55b9facd27d03
Mateus Mota <mateus.mota@cuponeria.com.br>
2022-07-28 01:44:45 -0300

    chore: transform all components to functions

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,30 +1,14 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import ReactModal from 'react-modal';
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
+const Modal = ({isOpen, setIsOpen, children}) => {
+  const [ modalStatus, setModalStatus ] = useState(isOpen);
 
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
-    }
-  }
+  useEffect(() => {
+    setModalStatus(isOpen)
+  }, [isOpen]);
 
-  componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
-
-    if (prevProps.isOpen !== isOpen) {
-      console.log(this.props)
-      this.setState({ modalStatus: isOpen })
-    }
-  }
-
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
-
-    return (
+  return (
       <ReactModal
         shouldCloseOnOverlayClick={!false}
         onRequestClose={setIsOpen}
@@ -51,8 +35,7 @@ class Modal extends Component {
       >
         {children}
       </ReactModal>
-    );
-  }
+  )
 };
 
 export default Modal;
